c7f5a4e0756bdd42760bf2c9bb2e460ba013e99c
extjcarrion-ux <ext_jcarrion@falabella.cl>
2026-06-24 10:35:38 -0400

    Update logging configuration and remove old log file; specify Python version in notebook

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+## main.ipynb
 import sys
-from src.utils.utils import leer_parquet
-from src.load.export_sentimientos_pbi import orquestador
+from src.load.export_sentimientos_pbi import (
+    orquestador
+)
+from config.config import get_settings
 
 def main():
-    orquestador()
-    # archivo = "sentimientos_pbi.parquet"
-    # parquet_file =leer_parquet(archivo)
-    # print(parquet_file)
+    settings = get_settings()
+    df = orquestador(
+            desde="2026-06-01",
+            hasta="2026-06-20"
+            )
+    print(df)
 
 if __name__ == "__main__":
     try:
