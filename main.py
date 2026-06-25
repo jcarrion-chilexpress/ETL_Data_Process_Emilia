6bb610bb86eaa5bfc9b1a381c997b41afa281bb3
extjcarrion-ux <ext_jcarrion@falabella.cl>
2026-06-25 16:25:18 -0400

    blucle tablas

--- main.py
+++ main.py
@@ -3,29 +3,31 @@ import sys
 import os
 from src.flow.flow import (step_get_sqlquery
                            ,step_generar_pdf
-                           ,step_create_table_Databricks)
-from src.utils.utils import leer_parquet
+                           ,step_save_table)
 from config.config import get_settings
+
+# from pyspark.sql import SparkSession
+# spark = SparkSession.builder.getOrCreate()
 
 os.system('cls')
 
 def main():
     settings = get_settings()
     json_file = settings.config_path
-    file_name = "sentimientos_emilia_dashboard" 
-    #"emilia_dashboard_base"
-    success,tabla_sql,query = step_get_sqlquery(file_name)
+    file_name = ["sentimientos_emilia_dashboard"
+                ,"emilia_dashboard_base"]
 
-    if success:
-        step_generar_pdf(query,file_name)
-    
-    df = leer_parquet(file_name+'.parquet')
-    step_create_table_Databricks(df,tabla_sql)
+    for file in file_name:
+        success,tabla_sql,query = step_get_sqlquery(file)
 
+        if success:
+            success, pdf = step_generar_pdf(query, file)
+        
+            if success:
+                step_save_table(spark, pdf, tabla_sql)
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
         sys.exit(130)
-
